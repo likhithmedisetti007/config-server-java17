116aa790e7e6d67d9c361f1072d52b2d8652cce9
Likhith Medisetti <likkyisalwayslucky@gmail.com>
2023-12-16 22:42:55 +0530

    Initial Commit

---
+++ src/main/java/com/likhith/config/ConfigServerJava17Application.java
@@ -0,0 +1,15 @@
+package com.likhith.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerJava17Application.class, args);
+	}
+
+}
